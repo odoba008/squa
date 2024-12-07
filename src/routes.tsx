e7043efa47f5eceb154c2b19426a8357493912fe
odoba008 <angelagray914@gmail.com>
2024-12-06 17:54:55 -0800

    amended

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/login'
 import ReLogin from './pages/re-login'
 import Success from './pages/success'
